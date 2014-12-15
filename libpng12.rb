669d00ca94d6d4d55ddf15a773728c13a5d90872
CarrotXin <hexin_2006@126.com>
2014-12-15 18:09:36 +0800

    Update libpng12.rb

--- libpng12.rb
+++ libpng12.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Libpng12 < Formula
   homepage "http://www.libpng.org/pub/png/libpng.html"
-  url "https://downloads.sourceforge.net/project/libpng/libpng12/1.2.52/libpng-1.2.52.tar.xz"
+  url "http://sourceforge.net/projects/libpng/files/libpng16/1.6.15/libpng-1.6.15.tar.xz/download"
   sha1 "8d19d762f836bb8900efc79ef8f1c3b64ee9f658"
 
   keg_only :provided_by_osx
